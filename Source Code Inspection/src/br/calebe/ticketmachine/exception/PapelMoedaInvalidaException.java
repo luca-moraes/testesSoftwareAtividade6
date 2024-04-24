7214f0313ab19d748124f90a01d626974c489d6f
luca-moraes <lcansd.lm@gmail.com>
2024-04-23 21:43:23 -0300

    Resolvendo o erro da issue #34

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,5 +5,10 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
-    
+    public PapelMoedaInvalidaException(String message) {
+        super(message);
+    }
+    public PapelMoedaInvalidaException() {
+        super("Valor de papel moeda inválido!");
+    }
 }
--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,10 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
+    public SaldoInsuficienteException() {
+        super("Erro de saldo insuficiente!");
+    }
 }
